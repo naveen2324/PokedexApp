6a719b562c7735b8fa09805962660cd52bf495f5
naveen_muthukumar <naveen3@illinois.edu>
2024-01-06 17:00:21 +0530

    Updates

---
+++ precache-manifest.0664d5276b87f2b5a79d6ef63b2edba0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cca768b2850096f38a455199b9eea1f",
+    "url": "/PokedexApp/index.html"
+  },
+  {
+    "revision": "f3595ac10c2298d3441f",
+    "url": "/PokedexApp/static/css/main.6a497689.chunk.css"
+  },
+  {
+    "revision": "f50b8812c0dd828eac04",
+    "url": "/PokedexApp/static/js/2.caf148ba.chunk.js"
+  },
+  {
+    "revision": "6a2a939c6c472481a460",
+    "url": "/PokedexApp/static/js/3.5a11cc32.chunk.js"
+  },
+  {
+    "revision": "f3595ac10c2298d3441f",
+    "url": "/PokedexApp/static/js/main.373c91c8.chunk.js"
+  },
+  {
+    "revision": "4206e27e2755ea0cd699",
+    "url": "/PokedexApp/static/js/runtime~main.6a732aca.js"
+  },
+  {
+    "revision": "b57559e8eea1e922c12ef55ff20f3741",
+    "url": "/PokedexApp/static/media/background.b57559e8.jpg"
+  },
+  {
+    "revision": "639198a608fe6ef7c9178507e6f2261e",
+    "url": "/PokedexApp/static/media/logo.639198a6.png"
+  }
+]);
